a684fe10289aadf70c9ad2463388b6df5f4e7a14
Komal Dhok <71183193+komal-1010@users.noreply.github.com>
2026-02-02 15:46:34 +0000

    updated db connection url

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -1,15 +1,18 @@
-import pg from 'pg';
-import dotenv from 'dotenv';
-dotenv.config();
+import pg from "pg";
 
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
+
+pool
+  .connect()
+  .then(() => console.log("✅ PostgreSQL connected"))
+  .catch(err => console.error("❌ DB init error:", err));
 
 export default pool;
